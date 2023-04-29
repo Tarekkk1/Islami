34174134b973f3c23ce377a41bba1a2fb28cedda
Tarek <tarekzeyad31@gmail.com>
2023-04-29 15:13:38 +0300

    geting started

--- lib/Taps/QuranTape.dart
+++ lib/Taps/QuranTape.dart
@@ -4,29 +4,136 @@ import 'package:islami/SuraArgs.dart';
 import 'package:islami/Taps/suraContintent.dart';
 
 class QuranTab extends StatelessWidget {
-List<String>suraNames=["الفاتحه","البقرة","آل عمران","النساء","المائدة","الأنعام","الأعراف","الأنفال","التوبة","يونس","هود"
-  ,"يوسف","الرعد","إبراهيم","الحجر","النحل","الإسراء","الكهف","مريم","طه","الأنبياء","الحج","المؤمنون"
-  ,"النّور","الفرقان","الشعراء","النّمل","القصص","العنكبوت","الرّوم","لقمان","السجدة","الأحزاب","سبأ"
-  ,"فاطر","يس","الصافات","ص","الزمر","غافر","فصّلت","الشورى","الزخرف","الدّخان","الجاثية","الأحقاف"
-  ,"محمد","الفتح","الحجرات","ق","الذاريات","الطور","النجم","القمر","الرحمن","الواقعة","الحديد","المجادلة"
-  ,"الحشر","الممتحنة","الصف","الجمعة","المنافقون","التغابن","الطلاق","التحريم","الملك","القلم","الحاقة","المعارج"
-  ,"نوح","الجن","المزّمّل","المدّثر","القيامة","الإنسان","المرسلات","النبأ","النازعات","عبس","التكوير","الإنفطار"
-  ,"المطفّفين","الإنشقاق","البروج","الطارق","الأعلى","الغاشية","الفجر","البلد","الشمس","الليل","الضحى","الشرح"
-  ,"التين","العلق","القدر","البينة","الزلزلة","العاديات","القارعة","التكاثر","العصر",
-  "الهمزة","الفيل","قريش","الماعون","الكوثر","الكافرون","النصر","المسد","الإخلاص","الفلق","الناس"];
+  List<String> suraNames = [
+    "الفاتحه",
+    "البقرة",
+    "آل عمران",
+    "النساء",
+    "المائدة",
+    "الأنعام",
+    "الأعراف",
+    "الأنفال",
+    "التوبة",
+    "يونس",
+    "هود",
+    "يوسف",
+    "الرعد",
+    "إبراهيم",
+    "الحجر",
+    "النحل",
+    "الإسراء",
+    "الكهف",
+    "مريم",
+    "طه",
+    "الأنبياء",
+    "الحج",
+    "المؤمنون",
+    "النّور",
+    "الفرقان",
+    "الشعراء",
+    "النّمل",
+    "القصص",
+    "العنكبوت",
+    "الرّوم",
+    "لقمان",
+    "السجدة",
+    "الأحزاب",
+    "سبأ",
+    "فاطر",
+    "يس",
+    "الصافات",
+    "ص",
+    "الزمر",
+    "غافر",
+    "فصّلت",
+    "الشورى",
+    "الزخرف",
+    "الدّخان",
+    "الجاثية",
+    "الأحقاف",
+    "محمد",
+    "الفتح",
+    "الحجرات",
+    "ق",
+    "الذاريات",
+    "الطور",
+    "النجم",
+    "القمر",
+    "الرحمن",
+    "الواقعة",
+    "الحديد",
+    "المجادلة",
+    "الحشر",
+    "الممتحنة",
+    "الصف",
+    "الجمعة",
+    "المنافقون",
+    "التغابن",
+    "الطلاق",
+    "التحريم",
+    "الملك",
+    "القلم",
+    "الحاقة",
+    "المعارج",
+    "نوح",
+    "الجن",
+    "المزّمّل",
+    "المدّثر",
+    "القيامة",
+    "الإنسان",
+    "المرسلات",
+    "النبأ",
+    "النازعات",
+    "عبس",
+    "التكوير",
+    "الإنفطار",
+    "المطفّفين",
+    "الإنشقاق",
+    "البروج",
+    "الطارق",
+    "الأعلى",
+    "الغاشية",
+    "الفجر",
+    "البلد",
+    "الشمس",
+    "الليل",
+    "الضحى",
+    "الشرح",
+    "التين",
+    "العلق",
+    "القدر",
+    "البينة",
+    "الزلزلة",
+    "العاديات",
+    "القارعة",
+    "التكاثر",
+    "العصر",
+    "الهمزة",
+    "الفيل",
+    "قريش",
+    "الماعون",
+    "الكوثر",
+    "الكافرون",
+    "النصر",
+    "المسد",
+    "الإخلاص",
+    "الفلق",
+    "الناس"
+  ];
 
   @override
-   Widget build(BuildContext context)  {
+  Widget build(BuildContext context) {
     return Center(
-      child: Column(   children: [     Image.asset("assets/images/quran_bg.png")
-          ,
-        Divider(
-          thickness: 2,
+      child: Column(
+        children: [
+          Image.asset("assets/images/quran_bg.png"),
+          Divider(
+            thickness: 2,
             color: Theme.of(context).primaryColor,
-        ),
-          Text("Sura Name ",textAlign: TextAlign.center,
-          style: Theme.of(context).textTheme.bodySmall),
-
+          ),
+          Text("Sura Name ",
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.bodySmall),
           Divider(
             thickness: 2,
             color: Theme.of(context).primaryColor,
@@ -35,37 +142,33 @@ List<String>suraNames=["الفاتحه","البقرة","آل عمران","الن
             thickness: 2,
             color: Theme.of(context).primaryColor,
           ),
-Expanded(
-  child:   ListView.separated(
-    separatorBuilder: (Context,index){
-    return Divider(
-      thickness: 1,
-      color: Theme.of(context).primaryColor,
-    );},
-
-    itemBuilder: (Context,index){
-    return 
-      InkWell(
-        onTap: (){
-          Navigator.pushNamed(context, SuraContent.routename,
-          arguments:suraArgs(suraName: suraNames[index], index: index) );
-
-        },
-        child: Center(
-          child: Text(suraNames[index],
-            style: Theme.of(context).textTheme.bodySmall,
-
-    ),
-        ),
-      );},
-  
-      itemCount: suraNames.length
-  
-  ),
-),
-      ],
+          Expanded(
+            child: ListView.separated(
+                separatorBuilder: (Context, index) {
+                  return Divider(
+                    thickness: 1,
+                    color: Theme.of(context).primaryColor,
+                  );
+                },
+                itemBuilder: (Context, index) {
+                  return InkWell(
+                    onTap: () {
+                      Navigator.pushNamed(context, SuraContent.routename,
+                          arguments: suraArgs(
+                              suraName: suraNames[index], index: index));
+                    },
+                    child: Center(
+                      child: Text(
+                        suraNames[index],
+                        style: Theme.of(context).textTheme.bodySmall,
+                      ),
+                    ),
+                  );
+                },
+                itemCount: suraNames.length),
+          ),
+        ],
       ),
     );
-
   }
 }
--- lib/home.dart
+++ lib/home.dart
@@ -6,66 +6,53 @@ import 'package:islami/Taps/RadioTab.dart';
 import 'package:islami/Taps/SebhaTab.dart';
 
 class HomeScreen extends StatefulWidget {
-static const String routename ="HomeScreen";
+  static const String routename = "HomeScreen";
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-int Index=0;
-List<Widget>tabs=[QuranTab(),
-RadioTab(),
-SebhaTab(),
-  AhadesTape()
-];
+  int Index = 0;
+  List<Widget> tabs = [QuranTab(), RadioTab(), SebhaTab(), AhadesTape()];
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-SizedBox(width: double.infinity,
-    child: Image.asset("assets/images/background.png",
-    fit: BoxFit.fill
-      ,)),
-
-        Scaffold(
-              appBar: AppBar(
-                title: Text(
-                  "Islami",style: Theme.of(context).textTheme.bodySmall
+        SizedBox(
+            width: double.infinity,
+            child: Image.asset(
+              "assets/images/background.png",
+              fit: BoxFit.fill,
             )),
-
-          body:tabs[Index]
-          ,
+        Scaffold(
+          appBar: AppBar(
+              title:
+                  Text("Islami", style: Theme.of(context).textTheme.bodySmall)),
+          body: tabs[Index],
           bottomNavigationBar: BottomNavigationBar(
-            currentIndex:Index,
-             onTap:(Value){
-              Index=Value;
-setState(() {
-
-});
-
-             },
-             items: [
-              BottomNavigationBarItem(icon: ImageIcon(
-                   AssetImage("assets/images/moshaf_blue.png")
-               ),label: "Moshaf"),
-
-               BottomNavigationBarItem(icon: ImageIcon(
-                   AssetImage("assets/images/sebha_blue.png")
-               ),label: "Sebha") , BottomNavigationBarItem(icon: ImageIcon(
-                   AssetImage("assets/images/radio_blue.png")
-               ),label: "Radio",backgroundColor: MyTheame.lightTheame.primaryColor),
-
-               BottomNavigationBarItem(icon: Icon(Icons.book
-               ),label: "Ahades") ,
-
-
-             ],
-
+            currentIndex: Index,
+            onTap: (Value) {
+              Index = Value;
+              setState(() {});
+            },
+            items: [
+              BottomNavigationBarItem(
+                  icon: ImageIcon(AssetImage("assets/images/moshaf_blue.png")),
+                  label: "Moshaf"),
+              BottomNavigationBarItem(
+                  icon: ImageIcon(AssetImage("assets/images/sebha_blue.png")),
+                  label: "Sebha"),
+              BottomNavigationBarItem(
+                  icon: ImageIcon(AssetImage("assets/images/radio_blue.png")),
+                  label: "Radio",
+                  backgroundColor: MyTheame.lightTheame.primaryColor),
+              BottomNavigationBarItem(icon: Icon(Icons.book), label: "Ahades"),
+            ],
           ),
         ),
       ],
     );
-
-  }}
+  }
+}
